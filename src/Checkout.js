c688c7d2b8d7fd60013bdcf75e9591c38cf50a14
Macuyler <macuyler@gmail.com>
2019-11-08 10:38:04 -0700

    Add mock data

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const data = {
+    '1': 0,
+    '2': 0,
+    '3': 2,
+    '5': 1,
+    '7': 0,
+    '90': 3,
+    't': 2,
+    'caps': 2,
+    'con': 0
+};
+
 const Checkout = () => (
     <div className="page-wrapper">
         <h1>Checkout Page</h1>
