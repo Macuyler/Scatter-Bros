dbd16c65ad6ef33f5f4946665a2e6e3c98075d21
Macuyler <macuyler@gmail.com>
2019-11-09 02:55:21 -0700

    Read Purchases database for data

--- src/Admin.js
+++ src/Admin.js
@@ -1,65 +1,99 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Button } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-const Admin = () => (
-  <div className="admin-page-wrapper">
-    <ExpansionPanel>
-      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-        <div className="expansion-summary">
-          <Typography variant="h6" component="h4">
-            charlie@brown.com
-          </Typography>
-          <Typography variant="h6" component="h4">
-            November 5, 2019
-          </Typography>
-          <Typography variant="h6" component="h4">
-            $3.50
-          </Typography>
-        </div>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails className="flex-column">
-        <div className="panel-flex-wrapper">
-          <div className="panel-address">
-            <p className="customer-name">Charlie Brown</p>
-            <p>1234 Fake Street</p>
-            <p>City, State, Zip</p>
-          </div>
-          <h2>White Cannon</h2>
-        </div>
-        <div className="admin-part-item">
-          <div className="admin-part-details">
-            <p className="admin-part-quantity">3</p>
-            <div className="admin-part-image-wrapper">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg" alt="pipe" />
-            </div>
-          </div>
-          <p className="admin-part-name">Inch Pipe</p>
-        </div>
+const pipeTypeToImage = {
+  pipe2: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+  pipe3: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+  pipe5: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+  pipe7: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+  pipe90:
+    "https://images.homedepot-static.com/productImages/30ce963e-181e-4659-97b7-947c40533f05/svn/white-dura-pvc-fittings-406-080-64_1000.jpg",
+  pipet: "https://mobileimages.lowes.com/product/converted/611942/611942124589.jpg?size=xl",
+  pipecap:
+    "https://images.homedepot-static.com/productImages/1251b43b-d36d-40ba-b060-9d00ab5c7de1/svn/white-charlotte-pipe-pvc-fittings-pvc021161000hd-64_1000.jpg",
+  pipecon: "https://mobileimages.lowes.com/product/converted/052063/052063444055.jpg?size=xl"
+};
 
-        <div className="admin-part-item">
-          <div className="admin-part-details">
-            <p className="admin-part-quantity">1</p>
-            <div className="admin-part-image-wrapper">
-              <img
-                src="https://images.homedepot-static.com/productImages/30ce963e-181e-4659-97b7-947c40533f05/svn/white-dura-pvc-fittings-406-080-64_1000.jpg"
-                alt="pipe"
-              />
+const pipeTypeToText = {
+  pipe2: "2 Inch Pipe",
+  pipe3: "3 Inch Pipe",
+  pipe5: "5 Inch Pipe",
+  pipe7: "7 Inch Pipe",
+  pipe90: "90-Degree Elbow",
+  pipet: "Tee Connector",
+  pipecap: "Cap",
+  pipecon: "Coupling"
+};
+
+const Admin = (props) => {
+  const [ purchases, setPurchases ] = useState([]);
+  useEffect(() => {
+    props.db.collection('purchases').get().then(pDocs => {
+      setPurchases(pDocs.docs.map(p => ({ ...p.data(), id: p.id, ref: p.ref })))
+    });
+  }, [props.db]);
+
+  const getPrice = (p) => {
+    let total = 5;
+    p.forEach(t => {
+      total += t.quantity * 0.5;
+    });
+    return total;
+  };
+
+  const toCurrency = amount => new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount);
+
+  return (
+    <div className="admin-page-wrapper">
+      {purchases.map((purchase, i) => (
+        <ExpansionPanel key={i}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+            <div className="expansion-summary">
+              <Typography variant="h6" component="h4">
+                {purchase.email}
+            </Typography>
+              <Typography variant="h6" component="h4">
+                {purchase.dateSubmitted}
+            </Typography>
+              <Typography variant="h6" component="h4">
+                {toCurrency(getPrice(purchase.parts))}
+            </Typography>
             </div>
-          </div>
-          <p className="admin-part-name">90-Degree Elbow</p>
-        </div>
-        <Button
-          type="submit"
-          style={{ width: "40%", margin: "30px 0 15px", marginLeft: "30%" }}
-          variant="contained"
-          color="primary"
-        >
-          Fullfilled
-        </Button>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
-  </div>
-);
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails className="flex-column">
+            <div className="panel-flex-wrapper">
+              <div className="panel-address">
+                <p className="customer-name">{`${purchase.fName} ${purchase.lName}`}</p>
+                <p>{purchase.address}</p>
+                <p>{`${purchase.city} ${purchase.state}, ${purchase.zip}`}</p>
+              </div>
+              <h2>{purchase.name}</h2>
+            </div>
+            {purchase.parts.map((part, j) => (
+              <div className="admin-part-item" key={`part${j}`}>
+                <div className="admin-part-details">
+                  <p className="admin-part-quantity">{part.quantity}</p>
+                  <div className="admin-part-image-wrapper">
+                    <img src={pipeTypeToImage[part.pipeType]} alt="pipe" />
+                  </div>
+                </div>
+                <p className="admin-part-name">{pipeTypeToText[part.pipeType]}</p>
+              </div>
+            ))}
+            <Button
+              type="submit"
+              style={{ width: "40%", margin: "30px 0 15px", marginLeft: "30%" }}
+              variant="contained"
+              color="primary"
+            >
+              Fullfilled
+          </Button>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      ))}
+    </div>
+  );
+};
 
 export default Admin;
