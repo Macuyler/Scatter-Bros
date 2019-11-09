55e8585a0aaa0d8262aef50c1f183e5390e698ea
Lora Klein <loraklein@Loras-MacBook-Pro.local>
2019-11-08 18:54:27 -0700

    initial checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,21 +1,76 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Paper, Typography, TextField } from "@material-ui/core";
+import cartData from "./data";
 
-const data = {
-    '1': 0,
-    '2': 0,
-    '3': 2,
-    '5': 1,
-    '7': 0,
-    '90': 3,
-    't': 2,
-    'caps': 2,
-    'con': 0
-};
+class Checkout extends Component {
+  render() {
+    console.log(new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(5));
 
-const Checkout = () => (
-    <div className="page-wrapper">
-        <h1>Checkout Page</h1>
-    </div>
-);
+    function toCurrency(amount) {
+      amount *= 0.5;
+      return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount);
+    }
+
+    return (
+      <div className="checkout-mainContent">
+        <div className="checkout-leftContent">
+          <Paper>
+            <div className="cart-content">
+              <Typography variant="h5" component="h3">
+                Shopping Cart
+              </Typography>
+              <div className="cart-header">
+                <p className="cart-heading">Item</p>
+                <div className="end-items-heading">
+                  <p className="cart-heading cart-quantity">Qty</p>
+                  <p className="cart-heading item-total">Item Total</p>
+                </div>
+              </div>
+              {cartData.map(item => {
+                if (item.qty > 0) {
+                  return (
+                    <div key={item.id} className="cart-item">
+                      <div className="cart-item-details">
+                        <div className="cart-item-image-wrapper">image</div>
+                        <p className="cart-item-name" key={item.id}>
+                          {item.name}
+                        </p>
+                      </div>
+                      <div className="end-items">
+                        <p className="cart-quantity">{item.qty}</p>
+                        <p className="item-total">{toCurrency(item.qty)}</p>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </Paper>
+        </div>
+        <div className="checkout-rightContent">
+          <Paper>
+            <form className="checkout-form">
+              <TextField
+                id="outlined-basic"
+                className="textField"
+                label="First Name"
+                margin="normal"
+                variant="outlined"
+              />
+
+              <TextField
+                id="outlined-basic"
+                className="textField"
+                label="Last Name"
+                margin="normal"
+                variant="outlined"
+              />
+            </form>
+          </Paper>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Checkout;
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 0,
+    name: "1 Inch Pipe",
+    qty: 0
+  },
+  {
+    id: 1,
+    name: "2 Inch Pipe",
+    qty: 0
+  },
+  {
+    id: 2,
+    name: "5 Inch Pipe",
+    qty: 2
+  },
+  {
+    id: 3,
+    name: "7 Inch Pipe",
+    qty: 0
+  },
+  {
+    id: 4,
+    name: "90-Degree Elbow",
+    qty: 1
+  },
+  {
+    id: 5,
+    name: "Tee Connector",
+    qty: 0
+  },
+  {
+    id: 6,
+    name: "Cap",
+    qty: 5
+  },
+  {
+    id: 7,
+    name: "Coupling",
+    qty: 0
+  }
+];
