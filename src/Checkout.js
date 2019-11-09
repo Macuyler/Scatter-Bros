a86aa251615c21f130a3e81512d0f4f8fd075985
Lora Klein <loraklein@Loras-MacBook-Pro.local>
2019-11-08 21:53:23 -0700

    get cart item data and style

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,15 +1,56 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router";
 import { Paper, Typography, TextField } from "@material-ui/core";
 import cartData from "./data";
 
 class Checkout extends Component {
   render() {
-    console.log(new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(5));
+    const carts = this.props.location.state;
 
-    function toCurrency(amount) {
+    const pipeNames = {
+      pipe2: "2 Inch Pipe",
+      pipe3: "3 Inch Pipe",
+      pipe5: "5 Inch Pipe",
+      pipe7: "7 Inch Pipe",
+      pipe90: "90-Degree Elbow",
+      pipet: "Tee Connector",
+      pipecap: "Cap",
+      pipecon: "Coupling"
+    };
+
+    const pipeImages = {
+      pipe2: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+      pipe3: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+      pipe5: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+      pipe7: "https://images-na.ssl-images-amazon.com/images/I/51YAw0xjeFL._SX466_.jpg",
+      pipe90:
+        "https://images.homedepot-static.com/productImages/30ce963e-181e-4659-97b7-947c40533f05/svn/white-dura-pvc-fittings-406-080-64_1000.jpg",
+      pipet: "https://mobileimages.lowes.com/product/converted/611942/611942124589.jpg?size=xl",
+      pipecap:
+        "https://images.homedepot-static.com/productImages/1251b43b-d36d-40ba-b060-9d00ab5c7de1/svn/white-charlotte-pipe-pvc-fittings-pvc021161000hd-64_1000.jpg",
+      pipecon: "https://mobileimages.lowes.com/product/converted/052063/052063444055.jpg?size=xl"
+    };
+
+    const getPipeName = pipe => {
+      return pipeNames[pipe];
+    };
+
+    const getPipeImage = pipe => {
+      return pipeImages[pipe];
+    };
+
+    const getSubtotal = () => {
+      let subtotal = 0;
+      carts.parts.map(item => {
+        subtotal += item.quantity;
+      });
+      return toCurrency(subtotal);
+    };
+
+    const toCurrency = amount => {
       amount *= 0.5;
       return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount);
-    }
+    };
 
     return (
       <div className="checkout-mainContent">
@@ -17,7 +58,7 @@ class Checkout extends Component {
           <Paper>
             <div className="cart-content">
               <Typography variant="h5" component="h3">
-                Shopping Cart
+                Items in your Kit
               </Typography>
               <div className="cart-header">
                 <p className="cart-heading">Item</p>
@@ -26,19 +67,19 @@ class Checkout extends Component {
                   <p className="cart-heading item-total">Item Total</p>
                 </div>
               </div>
-              {cartData.map(item => {
-                if (item.qty > 0) {
+              {carts.parts.map(item => {
+                if (item.quantity > 0) {
                   return (
-                    <div key={item.id} className="cart-item">
+                    <div key={item.pipeType} className="cart-item">
                       <div className="cart-item-details">
-                        <div className="cart-item-image-wrapper">image</div>
-                        <p className="cart-item-name" key={item.id}>
-                          {item.name}
-                        </p>
+                        <div className="cart-item-image-wrapper">
+                          <img src={getPipeImage(item.pipeType)} />
+                        </div>
+                        <p className="cart-item-name">{getPipeName(item.pipeType)}</p>
                       </div>
                       <div className="end-items">
-                        <p className="cart-quantity">{item.qty}</p>
-                        <p className="item-total">{toCurrency(item.qty)}</p>
+                        <p className="cart-quantity">{item.quantity}</p>
+                        <p className="item-total">{toCurrency(item.quantity)}</p>
                       </div>
                     </div>
                   );
@@ -49,25 +90,73 @@ class Checkout extends Component {
         </div>
         <div className="checkout-rightContent">
           <Paper>
+            <div className="item-subtotal">
+              <Typography variant="h5" component="h3">
+                Subtotal
+              </Typography>
+              <Typography variant="h5" component="h3">
+                {getSubtotal()}
+              </Typography>
+            </div>
             <form className="checkout-form">
-              <TextField
-                id="outlined-basic"
-                className="textField"
-                label="First Name"
-                margin="normal"
-                variant="outlined"
-              />
-
-              <TextField
-                id="outlined-basic"
-                className="textField"
-                label="Last Name"
-                margin="normal"
-                variant="outlined"
-              />
+              <div className="defaultFlex">
+                <div className="text-field-left">
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    className="textField"
+                    label="First Name"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </div>
+                <div className="text-field-right">
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    className="textField"
+                    label="Last Name"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </div>
+              </div>
+              <div className="defaultFlex">
+                <div className="text-field-left">
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    className="textField"
+                    label="City"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </div>
+                <div className="text-field-center">
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    className="textField"
+                    label="State"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </div>
+                <div className="text-field-right">
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    className="textField"
+                    label="Zip Code"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </div>
+              </div>
             </form>
           </Paper>
         </div>
+        {!carts || (carts && carts.parts.length === 0) ? <Redirect to={{ pathname: "/edit" }} /> : null}
       </div>
     );
   }
