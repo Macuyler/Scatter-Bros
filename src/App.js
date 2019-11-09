f33f8103b62b3ad42739deb5adee597b900222c4
Macuyler <macuyler@gmail.com>
2019-11-08 20:01:24 -0700

    Update home screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router';
 import Editor from './Editor';
 import Checkout from './Checkout';
 import Admin from './Admin';
+import { Typography, Button } from '@material-ui/core';
 
-const Home = () => (
-  <div className="page-wrapper">
-    <h1>Home</h1>
-  </div>
-)
+const Home = () => {
+  const [ go, setGo ] = useState(false);
+  return (
+    <div className="page-wrapper banner">
+      <Typography variant="h1" style={{ color: '#fff' }}>Scatter Bros</Typography>
+      <Typography variant="body1" style={{ color: '#fafafa', width: '50%' }}>Build Your Dream Marshmallow Launcher! This site lets you customize your very own PVC pipe marshmallow launcher. After you design your dream launcher we will ship the parts right to your door!</Typography>
+      <Button onClick={() => setGo(true)} variant="contained" color="primary" style={{ marginTop: 20, width: 300 }}>Get Started</Button>
+      {go ? <Redirect to="/edit" /> : null}
+    </div>
+  );
+};
 
 function App() {
   return (
